bc6c5658edd6f0af11195c5def60d5f33e8cea68
jagadee23 <gnaneswarajagadeeshreddy7385@gmail.com>
2024-02-05 00:25:43 +0530

    Stage 6 Completed

--- NITCbase/mynitcbase/Buffer/StaticBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/StaticBuffer.cpp
@@ -7,44 +7,86 @@ struct BufferMetaInfo StaticBuffer::metainfo[BUFFER_CAPACITY];
 
 StaticBuffer::StaticBuffer()
 {
-  // initialise all blocks as free
+  /*bufferIndex = 0 to BUFFER_CAPACITY-1*/
   for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++)
   {
     metainfo[bufferIndex].free = true;
+    metainfo[bufferIndex].dirty = false;
+    metainfo[bufferIndex].timeStamp = -1;
+    metainfo[bufferIndex].blockNum = -1;
   }
 }
 
+// write back all modified blocks on system exit
+StaticBuffer::~StaticBuffer()
+{
+  /*iterate through all the buffer blocks,
+    write back blocks with metainfo as free=false,dirty=true
+    using Disk::writeBlock()
+    */
+  for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++)
+  {
+    if (!metainfo[bufferIndex].free && metainfo[bufferIndex].dirty) {
+      Disk::writeBlock(blocks[bufferIndex], metainfo[bufferIndex].blockNum);
+    }
+  }
+}
 /*
 At this stage, we are not writing back from the buffer to the disk since we are
 not modifying the buffer. So, we will define an empty destructor for now. In
 subsequent stages, we will implement the write-back functionality here.
 */
-StaticBuffer::~StaticBuffer() {}
-
-int StaticBuffer::getFreeBuffer(int blockNum)
-{
-  if (blockNum < 0 || blockNum > DISK_BLOCKS)
-  {
+int StaticBuffer::getFreeBuffer(int blockNum){
+    // Check if blockNum is valid (non zero and less than DISK_BLOCKS)
+    // and return E_OUTOFBOUND if not valid.
+  if( blockNum < 0 || blockNum >= DISK_BLOCKS){
     return E_OUTOFBOUND;
   }
-  int allocatedBuffer;
+    // increase the timeStamp in metaInfo of all occupied buffers.
+for(int i=0;i<BUFFER_CAPACITY;i++){
+if(!metainfo[i].free){
+  metainfo[i].timeStamp++;
+}
+}
+    // let bufferNum be used to store the buffer number of the free/freed buffer.
+    int bufferNum= 0 , empty = 0;
 
-  // iterate through all the blocks in the StaticBuffer
-  // find the first free block in the buffer (check metainfo)
-  // assign allocatedBuffer = index of the free block
-  for (int i = 0; i < 32; i++)
+    // iterate through metainfo and check if there is any buffer free
+
+    // if a free buffer is available, set bufferNum = index of that free buffer.
+for (int i = 0; i < BUFFER_CAPACITY; i++)
+{
+  if(metainfo[i].free = true){
+    bufferNum = i;
+    break;
+  }
+}
+
+
+    // if a free buffer is not available,
+    //     find the buffer with the largest timestamp
+    //     IF IT IS DIRTY, write back to the disk using Disk::writeBlock()
+    //     set bufferNum = index of this buffer
+  for (int i = 0; !empty && i < BUFFER_CAPACITY; i++)
   {
-    if (metainfo[i].free == true)
+    if (metainfo[i].timeStamp > metainfo[bufferNum].timeStamp)
     {
-      allocatedBuffer = i;
-      break;
+      bufferNum = i;
     }
   }
-  if (allocatedBuffer == BUFFER_CAPACITY)
-    return FAILURE;
-  metainfo[allocatedBuffer].free = false;
-  metainfo[allocatedBuffer].blockNum = blockNum;
-  return allocatedBuffer;
+  if (metainfo[bufferNum].dirty)
+  {
+    Disk::writeBlock(blocks[bufferNum], metainfo[bufferNum].blockNum);
+    metainfo[bufferNum].dirty = false;
+  }
+    // update the metaInfo entry corresponding to bufferNum with
+    // free:false, dirty:false, blockNum:the input block number, timeStamp:0.
+metainfo[bufferNum].free = false;
+metainfo[bufferNum].dirty = false;
+metainfo[bufferNum].blockNum = blockNum;
+metainfo[bufferNum].timeStamp = 0;
+return bufferNum;
+    // return the bufferNum.
 }
 
 /* Get the buffer index where a particular block is stored
@@ -68,4 +110,31 @@ int StaticBuffer::getBufferNum(int blockNum)
   }
   // if block is not in the buffer
   return E_BLOCKNOTINBUFFER;
+}
+
+int StaticBuffer::setDirtyBit(int blockNum)
+{
+  // find the buffer index corresponding to the block using getBufferNum().
+  int bufferNum = StaticBuffer::getBufferNum(blockNum);
+  // if block is not present in the buffer (bufferNum = E_BLOCKNOTINBUFFER)
+  //     return E_BLOCKNOTINBUFFER
+  if (bufferNum == E_BLOCKNOTINBUFFER)
+  {
+    return E_BLOCKNOTINBUFFER;
+  }
+  // if blockNum is out of bound (bufferNum = E_OUTOFBOUND)
+  //     return E_OUTOFBOUND
+  if (bufferNum == E_OUTOFBOUND)
+  {
+    return E_OUTOFBOUND;
+  }
+  // else
+  //     (the bufferNum is valid)
+  //     set the dirty bit of that buffer to true in metainfo
+  else
+  {
+    metainfo[bufferNum].dirty = true;
+  }
+  // return SUCCESS
+  return SUCCESS;
 }
--- NITCbase/mynitcbase/Frontend/Frontend.cpp
+++ NITCbase/mynitcbase/Frontend/Frontend.cpp
@@ -23,14 +23,12 @@ int Frontend::close_table(char relname[ATTR_SIZE]) {
 }
 
 int Frontend::alter_table_rename(char relname_from[ATTR_SIZE], char relname_to[ATTR_SIZE]) {
-  // Schema::renameRel
-  return SUCCESS;
+  return Schema::renameRel(relname_from, relname_to);
 }
 
 int Frontend::alter_table_rename_column(char relname[ATTR_SIZE], char attrname_from[ATTR_SIZE],
                                         char attrname_to[ATTR_SIZE]) {
-  // Schema::renameAttr
-  return SUCCESS;
+  return Schema::renameAttr(relname, attrname_from, attrname_to);
 }
 
 int Frontend::create_index(char relname[ATTR_SIZE], char attrname[ATTR_SIZE]) {
